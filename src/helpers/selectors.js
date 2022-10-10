76f6ce8ce96db0dbc00471ba31df344a51f55b33
Alex Miller <alexannmill@gmail.com>
2022-10-10 16:22:37 -0700

    useVisul hook added, on cancel transition adn back

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -22,3 +22,33 @@ export function getInterview(state, interview) {
   copy.interviewer = foundInterviewer;
   return copy;
 }
+
+export function getInterviewersForDay(state, day) {
+  const interviewersArr = [];
+  const returnArr = [];
+  const filterDays = Object.values(state.days);
+  filterDays.forEach((arrDay) => {
+    if (arrDay.name === day) {
+      const arrAppointments = arrDay.appointments;
+      arrAppointments.forEach((id) => {
+        returnArr.push(state.appointments[id]);
+      });
+    }
+  });
+  const interviewsArr = [];
+  returnArr.forEach((app) => {
+    if (app.interview) {
+      interviewsArr.push(app.interview);
+    }
+  });
+  const interviewers = [];
+  interviewsArr.forEach((inter) => {
+    interviewers.push(inter.interviewer);
+  });
+  interviewers.forEach((id) => {
+    interviewersArr.push(state.interviewers[id]);
+  });
+  //remove duplicates
+  const final = [...new Set(interviewersArr)];
+  return final;
+}
